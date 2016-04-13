0f1a61fb8737cdeb74d360cec40a3f7da3b5d508
Sayan-Saha <sayan.saha.324@gmail.com>
2016-04-13 19:37:00 +0530

    first commit

---
+++ app/src/main/java/com/example/sayan/android_project/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.sayan.android_project;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void length(View view)
+    {
+        Intent next = new Intent(this,lengthActivity.class);
+        startActivity(next);
+    }
+    public void temp(View view)
+    {
+        Intent next = new Intent(this,temperature.class);
+        startActivity(next);
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/sayan/android_project/lengthActivity.java
@@ -0,0 +1,32 @@
+package com.example.sayan.android_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+public class lengthActivity extends AppCompatActivity {
+
+    Spinner dropDown;
+    ArrayAdapter<CharSequence> adapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_length);
+        dropDown = (Spinner) findViewById(R.id.dropDown);
+        adapter = ArrayAdapter.createFromResource(this,R.array.lenght_Unit,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        dropDown.setAdapter(adapter);
+        EditText lengthV = (EditText) findViewById(R.id.lengthValue);
+        String length = lengthV.getText().toString();
+
+
+
+    }
+
+}
